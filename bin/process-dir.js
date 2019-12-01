79714b7db1c3ab9bc4b16968882e6dbbbaa9cc96
George Campbell <g.campbell@zengenti.com>
2019-12-01 17:59:16 +0000

    get fs.stats for each file, log with chalk

--- bin/process-dir.js
+++ bin/process-dir.js
@@ -1,17 +1,28 @@
 #!/usr/bin/env node
 const processRecursively = require("../index.js").default;
+const chalk = require("chalk");
 
-processRecursively(
-  process.cwd(),
-  (filename, { level, fullPath }) => {
-    console.log("-".repeat(level) + filename);
-    if (true) {
-      if (filename.endsWith(".js")) {
-        console.log("found a js file at " + fullPath);
-      }
-    }
-  },
-  {
-    childrenFirst: true
+// Example usage of processRecursively:
+
+processRecursively(process.cwd(), (filename, { level, fullPath, stats }) => {
+  const isFile = stats.isFile();
+  const isJsFile = isFile && filename.endsWith(".js");
+  const isJsonFile = isFile && filename.endsWith(".json");
+  let msg = "-".repeat(level) + filename;
+  if (isJsFile) {
+    msg = chalk.yellow.inverse(msg);
+  } else if (isJsonFile) {
+    msg = chalk.yellow(msg);
+  } else if (isFile) {
+    msg = chalk.rgb(180, 160, 0)(msg);
+  } else {
+    msg += "/ processed.";
+    msg = chalk.rgb(120, 100, 0).italic(msg);
   }
-);
+  if (level === 1) {
+    msg = chalk.bold(msg + "\r\n");
+  }
+
+  console.log(msg);
+  //using fullPath you could apply some function to each file
+});
--- main.js
+++ main.js
@@ -2,27 +2,20 @@ const fs = require("fs");
 const path = require("path");
 export async function readPaths(pathstring) {
   return new Promise((resolve, reject) => {
-    const stats = fs.statSync(pathstring);
-    if (stats.isDirectory()) {
-      fs.readdir(pathstring, (err, names) => {
-        if (err) console.error("readdirerr", err);
-        if (err) reject(err);
-        // filter out filenames, only get folder names.
-        // const foldernames = names.filter(name => !name.includes("."));
-        resolve(names);
-      });
-    } else if (stats.isFile()) {
-      resolve([]);
-    } else {
-      throw new Error("neither file nor folder", pathstring, stats);
-    }
+    fs.readdir(pathstring, (err, names) => {
+      if (err) console.error("readdirerr", err);
+      if (err) reject(err);
+      names = names.sort();
+      // filter out filenames, only get folder names.
+      // const foldernames = names.filter(name => !name.includes("."));
+      resolve(names);
+    });
   });
 }
 
 async function* readPathsGenerator(pathstring) {
   try {
     const paths = await readPaths(pathstring);
-
     for (let pathname of paths) {
       yield pathname;
     }
@@ -38,25 +31,25 @@ export async function recursivelyReadPaths(
   level = 0
 ) {
   try {
-    let childrenFirst = options.childrenFirst;
     level++;
-    //for await... of requires Node.js 10+
-    for await (let pathname of readPathsGenerator(pathstring)) {
-      const fullPath = path.join(pathstring, pathname);
-      cb(pathname, { fullPath, pathstring, level });
-      const recurse = recursivelyReadPaths.bind(
-        this,
-        fullPath,
-        cb,
-        options,
-        level
-      );
-      if (childrenFirst) {
-        await recurse();
-      } else {
-        recurse();
+    const stats = fs.statSync(pathstring);
+    if (stats.isDirectory()) {
+      //for await... of requires Node.js 10+
+      for await (let pathname of readPathsGenerator(pathstring)) {
+        const fullPath = path.join(pathstring, pathname);
+
+        const recurse = recursivelyReadPaths.bind(
+          this,
+          fullPath,
+          cb,
+          options,
+          level
+        );
+        const stats = await recurse();
+        cb(pathname, { fullPath, pathstring, level, stats });
       }
     }
+    return stats;
   } catch (err) {
     throw err;
   }
